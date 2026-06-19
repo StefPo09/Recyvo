6df98733c03158a65f282ae436b917ced184c1fe
Stefan <stefan.popa@adv.ro>
2026-06-19 22:40:21 +0300

    Styled the Sign-Up Page (WIP)

--- frontend/app/SignUpPage/layout.tsx
+++ frontend/app/SignUpPage/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
