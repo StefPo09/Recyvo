03fe68c397ae9fc12501c79d9fbd7021f683f8a8
Stefan <stefan.popa@adv.ro>
2026-06-20 11:54:30 +0300

    Added Proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+    experimental: {
+        allowedDevOrigins: ['localhost', 'recyvo.adv.ro'],
+    },
 };
 
 export default nextConfig;
